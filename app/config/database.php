08327e9110166059be9ccad05ffce0a6ed934dc9
jebbo <jebbo@jebbo-VirtualBox>
2011-06-10 15:27:10 +0200

    prova da ambiente virtualizzato
    Signed-off-by: jebbo <jebbo@jebbo-VirtualBox>

--- app/config/database.php
+++ app/config/database.php
@@ -78,7 +78,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '',
+		'password' => 'mysql',
 		'database' => 'youris',
 		'prefix' => '',
 	);
